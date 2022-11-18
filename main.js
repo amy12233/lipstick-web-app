450266a1cd77a25104350468500a0a1ab83facd1
amy12233 <93597699+amy12233@users.noreply.github.com>
2022-11-18 10:40:10 -0600

    Add files via upload
    yes

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+
+noseX=0;
+noseY=0;
 function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
 }
+
 
 function setup() {
     canvas = createCanvas(300, 300);
@@ -17,6 +22,10 @@ function modelLoaded(){
 }
 function draw() {
     image(video, 0, 0, 300, 300);
+   // fill(255,0,0);
+    //stroke(255, 0 ,0);
+   // circle(noseX, noseY, 20);
+    image(clown_nose, noseX, noseY, 50, 30);
 }
 
 function take_snapshot(){
@@ -27,8 +36,10 @@ function gotPoses(results)
 {
     if(results.length > 0)
     {
+        noseX=results[0].pose.nose.x-20;
+        noseY=results[0].pose.nose.y+15;
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
